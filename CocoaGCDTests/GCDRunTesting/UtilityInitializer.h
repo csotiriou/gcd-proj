c044400d57ee85ac18d48059d073e9592c216028
Christos Sotiriou <csotiriou86@gmail.com>
2013-12-19 19:22:14 +0100

    — Changed some file location in order to facilitate Doxygen code generation

--- CocoaGCDTests/GCDRunTesting/UtilityInitializer.h
+++ CocoaGCDTests/GCDRunTesting/UtilityInitializer.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 /**
- Common functions for running unit tests and returning thingies. Useful for setting up logging, which is bundle-wide. This singleton is not tested ("how many tests would a unit test test if a uni test could test tests?")
+ Common functions for running unit tests and returning thingies. Useful for setting up logging both in the main application and the unit tests, which is bundle-wide. This singleton is not tested ("how many tests would a unit test test if a unit test could test tests?")
  Design decision: This kind of functionality is implemented using a singleton, because we are not sure about when each unit test is going to run. Therefore, we need a mechanism to hold global variables, and not re-initialize loggers and other data more than once.
  */
 @interface UtilityInitializer : NSObject
